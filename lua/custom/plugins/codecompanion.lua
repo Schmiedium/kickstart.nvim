5bd67dfa312ab6bc1b4f347713fb09869ce18be1
Alex Eisenschmied <105024964+Schmiedium@users.noreply.github.com>
2025-06-06 11:24:31 -0400

    Needed to readd api key to actually use this plugin, also added keybinds so I don't go insane!

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -4,17 +4,38 @@ return {
     opts = {
       log_level = 'DEBUG', -- or "TRACE"
     },
+    adapters = {
+      gemini = function()
+        return require('codecompanion.adapters').extend('gemini', {
+          env = { api_key = 'cmd: cat ~/.config/gemini-api-key' },
+        })
+      end,
+    },
     strategies = {
       chat = {
-        adapter = 'gemini',
+        adapter = { name = 'gemini', model = 'gemini-2.5-flash-preview-05-20' },
       },
       inline = {
-        adapter = 'gemini',
+        adapter = { name = 'gemini', model = 'gemini-2.5-flash-preview-05-20' },
+        keymaps = {
+          accept_change = {
+            modes = { n = 'ga' },
+            description = 'Accept the suggested change',
+          },
+          reject_change = {
+            modes = { n = 'gR' },
+            description = 'Reject the suggested change',
+          },
+        },
       },
     },
   },
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
+  },
+  keys = {
+    { '<leader>ccc', '<cmd>CodeCompanionChat<cr>', desc = 'CodeCompanionChat' },
+    { '<leader>cct', '<cmd>CodeCompanionChat Toggle<cr>', desc = 'CodeCompanionChat Toggle' },
   },
 }
